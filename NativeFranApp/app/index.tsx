f7886743d43ffdae8068dc8a97d9ff4a53d73ee5
xavier1195 <xavier1195@gmail.com>
2025-04-06 15:24:47 -0400

    changes test

---
+++ NativeFranApp/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, World why are you here!</Text>
+    </View>
+  );
+};
+
+export default App;
