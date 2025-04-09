cbfeee40cd59fc29a6b39fc135d568477cc3f359
xavier1195 <xavier1195@gmail.com>
2025-04-08 21:05:45 -0400

    correct app using JS

---
+++ FransReactApp/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, World this is Francis Poeske!</Text>
+    </View>
+  );
+};
+
+export default App;
